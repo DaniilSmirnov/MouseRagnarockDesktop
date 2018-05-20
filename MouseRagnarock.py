222e071e71e041bf6324652b5a1dd48de11d56b3
DaniilSmirnov <littlebigcrafting@gmail.com>
2018-05-20 18:43:15 +0300

    Введено количество сыра и мышь Эщкере

--- MouseRagnarock.py
+++ MouseRagnarock.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 from PyQt5 import QtCore, QtGui, QtWidgets
-#from pymsgbox import *
+from pymsgbox import *
 from xml.dom import minidom
 from random import *
 from threading import Thread
@@ -11,68 +11,60 @@ energy_max = 100
 
 
 class Ui_MainWindow(object):
-
     def setupUi(self, MainWindow):
-
         MainWindow.setObjectName("MainWindow")
         MainWindow.resize(795, 600)
         self.centralwidget = QtWidgets.QWidget(MainWindow)
         self.centralwidget.setObjectName("centralwidget")
-        self.widget = QtWidgets.QWidget(self.centralwidget)
-        self.widget.setGeometry(QtCore.QRect(0, 0, 791, 551))
-        self.widget.setObjectName("widget")
-        self.gridLayout = QtWidgets.QGridLayout(self.widget)
+        self.layoutWidget = QtWidgets.QWidget(self.centralwidget)
+        self.layoutWidget.setGeometry(QtCore.QRect(0, 0, 791, 551))
+        self.layoutWidget.setObjectName("layoutWidget")
+        self.gridLayout = QtWidgets.QGridLayout(self.layoutWidget)
         self.gridLayout.setContentsMargins(0, 0, 0, 0)
         self.gridLayout.setObjectName("gridLayout")
-        self.devicesbox = QtWidgets.QGroupBox(self.widget)
+        self.devicesbox = QtWidgets.QGroupBox(self.layoutWidget)
         self.devicesbox.setObjectName("devicesbox")
         self.gridLayout_4 = QtWidgets.QGridLayout(self.devicesbox)
         self.gridLayout_4.setObjectName("gridLayout_4")
-        self.label_2 = QtWidgets.QLabel(self.devicesbox)
-        self.label_2.setObjectName("label_2")
-        self.gridLayout_4.addWidget(self.label_2, 0, 0, 1, 1)
-        self.label_3 = QtWidgets.QLabel(self.devicesbox)
-        self.label_3.setObjectName("label_3")
-        self.gridLayout_4.addWidget(self.label_3, 1, 0, 1, 1)
-        self.label_4 = QtWidgets.QLabel(self.devicesbox)
-        self.label_4.setObjectName("label_4")
-        self.gridLayout_4.addWidget(self.label_4, 2, 0, 1, 1)
-        self.label_2.raise_()
-        self.label_3.raise_()
-        self.label_4.raise_()
+        self.cheese_label = QtWidgets.QLabel(self.devicesbox)
+        self.cheese_label.setObjectName("cheese_label")
+        self.gridLayout_4.addWidget(self.cheese_label, 0, 0, 1, 1)
+        self.board_label = QtWidgets.QLabel(self.devicesbox)
+        self.board_label.setObjectName("board_label")
+        self.gridLayout_4.addWidget(self.board_label, 1, 0, 1, 1)
+        self.device_label = QtWidgets.QLabel(self.devicesbox)
+        self.device_label.setObjectName("device_label")
+        self.gridLayout_4.addWidget(self.device_label, 2, 0, 1, 1)
         self.gridLayout.addWidget(self.devicesbox, 5, 3, 1, 1)
-        self.MoneyBox = QtWidgets.QGroupBox(self.widget)
+        self.MoneyBox = QtWidgets.QGroupBox(self.layoutWidget)
         self.MoneyBox.setObjectName("MoneyBox")
         self.gridLayout_2 = QtWidgets.QGridLayout(self.MoneyBox)
         self.gridLayout_2.setObjectName("gridLayout_2")
         self.moneylabel = QtWidgets.QLabel(self.MoneyBox)
-        self.moneylabel.setObjectName("label")
+        self.moneylabel.setObjectName("moneylabel")
         self.gridLayout_2.addWidget(self.moneylabel, 1, 0, 1, 1)
         self.diamondslabel = QtWidgets.QLabel(self.MoneyBox)
-        self.diamondslabel.setObjectName("label_7")
+        self.diamondslabel.setObjectName("diamondslabel")
         self.gridLayout_2.addWidget(self.diamondslabel, 2, 0, 1, 1)
-        self.diamondslabel.raise_()
-        self.moneylabel.raise_()
-        self.diamondslabel.raise_()
         self.gridLayout.addWidget(self.MoneyBox, 0, 3, 1, 1)
-        self.switchbox = QtWidgets.QGroupBox(self.widget)
+        self.switchbox = QtWidgets.QGroupBox(self.layoutWidget)
         self.switchbox.setObjectName("switchbox")
         self.gridLayout_5 = QtWidgets.QGridLayout(self.switchbox)
         self.gridLayout_5.setObjectName("gridLayout_5")
-        self.pushButton_5 = QtWidgets.QPushButton(self.switchbox)
-        self.pushButton_5.setEnabled(False)
-        self.pushButton_5.setObjectName("pushButton_5")
-        self.gridLayout_5.addWidget(self.pushButton_5, 0, 0, 1, 1)
-        self.pushButton_7 = QtWidgets.QPushButton(self.switchbox)
-        self.pushButton_7.setEnabled(False)
-        self.pushButton_7.setObjectName("pushButton_7")
-        self.gridLayout_5.addWidget(self.pushButton_7, 1, 0, 1, 1)
-        self.pushButton_8 = QtWidgets.QPushButton(self.switchbox)
-        self.pushButton_8.setEnabled(False)
-        self.pushButton_8.setObjectName("pushButton_8")
-        self.gridLayout_5.addWidget(self.pushButton_8, 2, 0, 1, 1)
+        self.cheese_button = QtWidgets.QPushButton(self.switchbox)
+        self.cheese_button.setEnabled(False)
+        self.cheese_button.setObjectName("cheese_button")
+        self.gridLayout_5.addWidget(self.cheese_button, 0, 0, 1, 1)
+        self.board_button = QtWidgets.QPushButton(self.switchbox)
+        self.board_button.setEnabled(False)
+        self.board_button.setObjectName("board_button")
+        self.gridLayout_5.addWidget(self.board_button, 1, 0, 1, 1)
+        self.device_button = QtWidgets.QPushButton(self.switchbox)
+        self.device_button.setEnabled(False)
+        self.device_button.setObjectName("device_button")
+        self.gridLayout_5.addWidget(self.device_button, 2, 0, 1, 1)
         self.gridLayout.addWidget(self.switchbox, 5, 2, 1, 1)
-        self.mouseview = QtWidgets.QGroupBox(self.widget)
+        self.mouseview = QtWidgets.QGroupBox(self.layoutWidget)
         sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Minimum, QtWidgets.QSizePolicy.Preferred)
         sizePolicy.setHorizontalStretch(0)
         sizePolicy.setVerticalStretch(0)
@@ -99,7 +91,7 @@ class Ui_MainWindow(object):
         self.mouseattachment.setObjectName("mouseattachment")
         self.gridLayout_6.addWidget(self.mouseattachment, 2, 0, 1, 2)
         self.gridLayout.addWidget(self.mouseview, 7, 2, 1, 2)
-        self.graphicsView = QtWidgets.QGraphicsView(self.widget)
+        self.graphicsView = QtWidgets.QGraphicsView(self.layoutWidget)
         sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Preferred, QtWidgets.QSizePolicy.Preferred)
         sizePolicy.setHorizontalStretch(0)
         sizePolicy.setVerticalStretch(0)
@@ -107,11 +99,11 @@ class Ui_MainWindow(object):
         self.graphicsView.setSizePolicy(sizePolicy)
         self.graphicsView.setObjectName("graphicsView")
         self.gridLayout.addWidget(self.graphicsView, 5, 0, 3, 2)
-        self.clans = QtWidgets.QPushButton(self.widget)
+        self.clans = QtWidgets.QPushButton(self.layoutWidget)
         self.clans.setEnabled(False)
         self.clans.setObjectName("clans")
         self.gridLayout.addWidget(self.clans, 4, 3, 1, 1)
-        self.pipe = QtWidgets.QPushButton(self.widget)
+        self.pipe = QtWidgets.QPushButton(self.layoutWidget)
         sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Preferred, QtWidgets.QSizePolicy.Preferred)
         sizePolicy.setHorizontalStretch(0)
         sizePolicy.setVerticalStretch(0)
@@ -120,22 +112,23 @@ class Ui_MainWindow(object):
         self.pipe.setMaximumSize(QtCore.QSize(16777215, 16777215))
         self.pipe.setObjectName("pipe")
         self.gridLayout.addWidget(self.pipe, 0, 1, 1, 2)
-        self.energybar = QtWidgets.QProgressBar(self.widget)
+        self.energybar = QtWidgets.QProgressBar(self.layoutWidget)
         self.energybar.setProperty("value", 10)
         self.energybar.setObjectName("energybar")
         self.gridLayout.addWidget(self.energybar, 2, 1, 1, 2)
-        self.move = QtWidgets.QPushButton(self.widget)
+        self.move = QtWidgets.QPushButton(self.layoutWidget)
+        self.move.setEnabled(False)
         self.move.setObjectName("move")
         self.gridLayout.addWidget(self.move, 4, 1, 1, 1)
-        self.shop = QtWidgets.QPushButton(self.widget)
+        self.shop = QtWidgets.QPushButton(self.layoutWidget)
         self.shop.setEnabled(False)
         self.shop.setObjectName("shop")
         self.gridLayout.addWidget(self.shop, 4, 2, 1, 1)
-        self.inventory = QtWidgets.QPushButton(self.widget)
+        self.inventory = QtWidgets.QPushButton(self.layoutWidget)
         self.inventory.setEnabled(False)
         self.inventory.setObjectName("inventory")
         self.gridLayout.addWidget(self.inventory, 4, 0, 1, 1)
-        self.filler = QtWidgets.QLabel(self.widget)
+        self.filler = QtWidgets.QLabel(self.layoutWidget)
         self.filler.setEnabled(True)
         sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Expanding)
         sizePolicy.setHorizontalStretch(0)
@@ -145,7 +138,6 @@ class Ui_MainWindow(object):
         self.filler.setText("")
         self.filler.setObjectName("filler")
         self.gridLayout.addWidget(self.filler, 6, 2, 1, 2)
-        self.mouseview.raise_()
         MainWindow.setCentralWidget(self.centralwidget)
         self.menubar = QtWidgets.QMenuBar(MainWindow)
         self.menubar.setGeometry(QtCore.QRect(0, 0, 795, 21))
@@ -158,23 +150,25 @@ class Ui_MainWindow(object):
         self.retranslateUi(MainWindow)
         QtCore.QMetaObject.connectSlotsByName(MainWindow)
 
+        self.update_ui()
+
     def retranslateUi(self, MainWindow):
         _translate = QtCore.QCoreApplication.translate
         MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
         self.devicesbox.setTitle(_translate("MainWindow", "Devices"))
-        self.label_2.setText(_translate("MainWindow", "TextLabel"))
-        self.label_3.setText(_translate("MainWindow", "TextLabel"))
-        self.label_4.setText(_translate("MainWindow", "TextLabel"))
+        self.cheese_label.setText(_translate("MainWindow", "TextLabel"))
+        self.board_label.setText(_translate("MainWindow", "TextLabel"))
+        self.device_label.setText(_translate("MainWindow", "TextLabel"))
         self.MoneyBox.setTitle(_translate("MainWindow", "Money"))
-        self.moneylabel.setText(_translate("MainWindow", " "))
-        self.diamondslabel.setText(_translate("MainWindow", " "))
+        self.moneylabel.setText(_translate("MainWindow", "TextLabel"))
+        self.diamondslabel.setText(_translate("MainWindow", "TextLabel"))
         self.switchbox.setTitle(_translate("MainWindow", "Devices"))
-        self.pushButton_5.setText(_translate("MainWindow", "PushButton"))
-        self.pushButton_7.setText(_translate("MainWindow", "PushButton"))
-        self.pushButton_8.setText(_translate("MainWindow", "PushButton"))
+        self.cheese_button.setText(_translate("MainWindow", "Cheese"))
+        self.board_button.setText(_translate("MainWindow", "Board"))
+        self.device_button.setText(_translate("MainWindow", "Device"))
         self.mouseview.setTitle(_translate("MainWindow", "Journal"))
-        self.mousecost.setText(_translate("MainWindow", " "))
-        self.mousename.setText(_translate("MainWindow", " "))
+        self.mousecost.setText(_translate("MainWindow", "TextLabel"))
+        self.mousename.setText(_translate("MainWindow", "TextLabel"))
         self.mouseattachment.setText(_translate("MainWindow", "TextLabel"))
         self.clans.setText(_translate("MainWindow", "Clans"))
         self.pipe.setText(_translate("MainWindow", "pipe"))
@@ -185,7 +179,7 @@ class Ui_MainWindow(object):
         self.pipe.clicked.connect(self.pipe_click)
 
     def catch_mouse(self):
-        global money
+        global money, cheese_amount
 
         number = 1 + int(random()*10)
 
@@ -193,22 +187,24 @@ class Ui_MainWindow(object):
         self.mousecost.setText(GameLogic.ReadXML(self, "mice", "cost", number, 0))
         money += int(GameLogic.ReadXML(self, "mice", "cost", number, 0))
         self.moneylabel.setText(str(money))
+        if money !=10:
+            cheese_amount-=1
 
     def update_ui(self):
 
         global energy
         self.energybar.setValue(energy)
+        self.cheese_label.setText("Cheese: "+str(cheese_amount))
 
     def pipe_click(self):
 
         global energy, energy_max
 
-        if energy > 0:
+        if energy > 0 and cheese_amount > 0:
             energy -= 1
             self.catch_mouse()
         else:
-            self.diamondslabel.setText("Not enough energy")
-            #alert(text='Not enough energy', title='Energsy info', button='OK')
+            alert(text='Not enough energy or cheese', title='Alert', button='OK')
 
         if energy < energy_max:
             thread = EnergyThread()
@@ -219,10 +215,33 @@ class Ui_MainWindow(object):
 
 class GameLogic(object):
 
+    global energy, money
+
     def ReadXML(self, tag, name, number, index):
         xmldoc = minidom.parse('locations.xml')
         itemlist = xmldoc.getElementsByTagName(str(tag) + str(number))
         return itemlist[index].attributes[str(name)].value
+
+    def ReadFile(self, position, index):
+        xmldoc = minidom.parse('userdata.xml')
+        itemlist = xmldoc.getElementsByTagName(str("user"))
+        return itemlist[index].attributes[position].value
+
+    def WriteFile(self):
+        doc, tag, text = Doc().tagtext()
+        with tag('user', energy=str(energy), money=str(money), cheese_amount=str(cheese_amount)):
+            text(str(energy))
+
+        result = indent(
+            doc.getvalue(),
+            indentation=' ' * 4,
+            newline='\r\n')
+
+        filename = 'userdata.xml'
+        myfile = open(filename, 'w')
+        myfile.write(result)
+        myfile.close()
+        print(result)
 
 
 class EnergyThread(Thread):
@@ -239,31 +258,9 @@ class EnergyThread(Thread):
         #Ui_MainWindow.energybar.setText(str(energy))
 
 
-def ReadFile(position, index):
-    xmldoc = minidom.parse('userdata.xml')
-    itemlist = xmldoc.getElementsByTagName(str("user"))
-    return itemlist[index].attributes[position].value
-
-
-def WriteFile():
-    doc, tag, text = Doc().tagtext()
-    with tag('user', energy=str(energy), money=str(money)):
-        text(str(energy))
-
-    result = indent(
-        doc.getvalue(),
-        indentation=' ' * 4,
-        newline='\r\n')
-
-    filename = 'userdata.xml'
-    myfile = open(filename, 'w')
-    myfile.write(result)
-    myfile.close()
-    print(result)
-
-
-energy = int(ReadFile("energy", 0))
-money = int(ReadFile("money", 0))
+energy = int(GameLogic.ReadFile(GameLogic, "energy", 0))
+money = int(GameLogic.ReadFile(GameLogic, "money", 0))
+cheese_amount = int(GameLogic.ReadFile(GameLogic, "cheese_amount", 0))
 
 if __name__ == "__main__":
     import sys
@@ -272,4 +269,4 @@ if __name__ == "__main__":
     ui = Ui_MainWindow()
     ui.setupUi(MainWindow)
     MainWindow.show()
-    sys.exit(app.exec_(), WriteFile())
+    sys.exit(app.exec_(), GameLogic.WriteFile(GameLogic))
